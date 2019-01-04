4901e93fab6a34f6873db7f9b97c50468312f41e
alessanderleite <alessander.logistica@gmail.com>
2019-01-04 07:25:04 -0200

    code the onCreate, onResume and onPause methods in the HeadingAndRotationActivity class

--- app/src/main/java/br/com/alessanderleite/headingandrotation/HeadingAndRotationActivity.java
+++ app/src/main/java/br/com/alessanderleite/headingandrotation/HeadingAndRotationActivity.java
@@ -1,12 +1,54 @@
 package br.com.alessanderleite.headingandrotation;
 
 import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 
+/*
+* This is the entry point to the game.
+* It will handle the lifecycle of the game by calling
+* methods of view when prompted to so by OS
+*/
 public class HeadingAndRotationActivity extends Activity {
+
+    /*
+    * view will be the view of the game
+    * It will also hold the logic of the game
+    * and respond to screen touches as well
+    */
+    HeadingAndRotationView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Get a Display object to access screen details
+        Display display = getWindowManager().getDefaultDisplay();
+        // Load the resolution into a Point object
+        Point size = new Point();
+        display.getSize(size);
+
+        // Initialize gameView and set it as the view
+        view = new HeadingAndRotationView(this, size.x, size.y);
+        setContentView(view);
+    }
+
+    // This method executes when the player starts the game
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Tell the gameView resume method to execute
+        view.resume();
+    }
+
+    // This method executes when the player quits the game
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Tell the gameView pause method to execute
+        view.pause();
     }
 }
