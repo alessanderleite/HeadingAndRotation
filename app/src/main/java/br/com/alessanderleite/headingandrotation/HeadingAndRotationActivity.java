41ebca0ec4b9e48c0e2219ebd8d94fc7dcfcb38a
alessanderleite <alessander.logistica@gmail.com>
2019-01-03 22:02:55 -0200

    initial commit

---
+++ app/src/main/java/br/com/alessanderleite/headingandrotation/HeadingAndRotationActivity.java
@@ -0,0 +1,12 @@
+package br.com.alessanderleite.headingandrotation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HeadingAndRotationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
