a421c183e2f29dba46aab3c568a98c1ff80743cc
alessanderleite <alessander.logistica@gmail.com>
2019-01-05 11:10:56 -0200

    add the draw method inside the HeadingAndRotationView class

--- app/src/main/java/br/com/alessanderleite/headingandrotation/HeadingAndRotationView.java
+++ app/src/main/java/br/com/alessanderleite/headingandrotation/HeadingAndRotationView.java
@@ -2,6 +2,7 @@ package br.com.alessanderleite.headingandrotation;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -58,6 +59,44 @@ public class HeadingAndRotationView extends SurfaceView implements Runnable {
 
         // Move the player's ship
         ship.update(fps);
+    }
+
+    private void draw() {
+        // Make sure our drawing surface is valid or we crash
+        if (ourHolder.getSurface().isValid()) {
+            // Lock the canvas ready to draw
+            canvas = ourHolder.lockCanvas();
+
+            // Draw the background color
+            canvas.drawColor(Color.argb(255,26,128,182));
+
+            // Choose the brush color for drawing
+            paint.setColor(Color.argb(255,255,255,255));
+
+            // Now draw the player spaceship
+            // Line from a to b
+            canvas.drawLine(ship.getA().x, ship.getA().y,
+                    ship.getB().x, ship.getB().y,
+                    paint);
+
+            // Line from b to c
+            canvas.drawLine(ship.getB().x, ship.getB().y,
+                    ship.getC().x, ship.getC().y,
+                    paint);
+
+            // Line from c to a
+            canvas.drawLine(ship.getC().x, ship.getC().y,
+                    ship.getA().x, ship.getA().y,
+                    paint);
+
+            canvas.drawPoint(ship.getCentre().x, ship.getCentre().y, paint);
+
+            paint.setTextSize(60);
+            canvas.drawText("facingAngle = " + (int)ship.getFacingAngle() + " degrees", 20, 70, paint);
+
+            // Draw everything to the screen
+            ourHolder.unlockCanvasAndPost(canvas);
+        }
     }
 
     // If the Activity is paused/stopped
